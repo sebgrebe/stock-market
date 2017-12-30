201f58ad67997fdbc5734be66d6e38f4d7c0deb6
sebgrebe <sebbecker@gmx.net>
2017-12-30 15:49:52 +0100

    setting up project

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+registerServiceWorker();
