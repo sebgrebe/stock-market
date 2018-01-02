35afdb0b111c3a160af6bf7cfa910aede382167a
sebgrebe <sebbecker@gmx.net>
2018-01-02 12:10:36 +0000

    add websocket server

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
+const socket = new WebSocket("ws://localhost:8080")
+// socket.addEventListener('open',() => {
+//     socket.send('hello')
+// })
+
+
 render(
     <Provider>
         <App />
